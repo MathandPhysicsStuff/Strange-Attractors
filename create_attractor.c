e233b7874ccf3015e7656b7b8946d839373583b8
MathandPhysicsStuff <emailforgithub51@protonmail.com>
2024-01-05 12:50:03 -0500

    test

---
+++ create_attractor.c
@@ -0,0 +1,30 @@
+#include "header.h"
+
+//c is an array of constants
+int find_attractor(double *c, c_len)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- header.h
+++ header.h
@@ -9,8 +9,11 @@
 #include "SDL2/SDL.h"
 
 //create_attractor.c functions
+int find_attractor(double *c, int c_len);
 
 //predefined_attractors.c functions
+
+//render_attractor.c functions
 
 //matrix_math.c functions
 
