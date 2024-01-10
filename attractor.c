901bd1cf28088ce4c5397b61aaec12ecd02ff216
MathandPhysicsStuff <emailforgithub51@protonmail.com>
2024-01-10 11:50:54 -0500

    can store attractor coefficients in binary file (has bugs)

--- attractor.c
+++ attractor.c
@@ -43,15 +43,25 @@ int main()
         .scale = 150 
     };
 
+    FILE *write_attractor = fopen("cool_attractors.bin", "ab");
+    FILE *read_attractor = fopen("cool_attractors.bin", "rb");
+    if (write_attractor == NULL || read_attractor == NULL)
+    {
+        printf("file not found\n");
+        exit(1);
+    }
+
     int c_len = 12;
     int number_of_points = 100000;
     double c[c_len];
     double c_copy[c_len]; 
+    double c_buffer[c_len]; 
     Point points[number_of_points];
 
     double shift = 0;
     int animate = FALSE;
     int slide_show = FALSE;
+    int b = 0;
    
     /*^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^*/
     /*||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||*/
@@ -74,30 +84,65 @@ int main()
             {
                 switch (event.key.keysym.sym)
                 {
+                    //exit program
                     case SDLK_ESCAPE:
                         running = SDL_FALSE;
                         break;
-
+                    
+                    //generate new attractor
                     case SDLK_RIGHT:
                         find_2d_attractor(c, c_len, 2);
                         create_2d_attractor(points, c, number_of_points);
                         memcpy(c_copy, c, sizeof(c));
                         animate = FALSE; 
                         break; 
-  
+                    
+                    //start slide show of attractors 
+                    case SDLK_UP:
+                        slide_show = TRUE; 
+                        break;
+                    
+                    //reset attractor 
                     case SDLK_r:
                         memcpy(c, c_copy, sizeof(c_copy));
                         create_2d_attractor(points, c, number_of_points);
                         slide_show = FALSE; 
                         animate = FALSE; 
                         break;
-
+                    
+                    //save attractor to text file
                     case SDLK_s:
-                        slide_show = TRUE; 
+                        fwrite(c, sizeof(double), c_len, write_attractor);
                         break;
-
+                    
+                    //animate attractor
                     case SDLK_a:
                         animate = TRUE; 
+                        break; 
+
+                    case SDLK_m:
+                        fread(c_buffer, sizeof(double), c_len, read_attractor);
+
+                        fseek(read_attractor, b, SEEK_SET);
+                        b += sizeof(double)*c_len;
+
+                        memcpy(c, c_buffer, sizeof(c_buffer));
+                        create_2d_attractor(points, c, number_of_points);
+
+                        animate = FALSE; 
+                        break; 
+
+                    case SDLK_n:
+
+                        fseek(read_attractor, 0, SEEK_SET);
+                        b = 0; // sizeof(double)*c_len;
+
+                        fread(c_buffer, sizeof(double), c_len, read_attractor);
+
+                        memcpy(c, c_buffer, sizeof(c_buffer));
+                        create_2d_attractor(points, c, number_of_points);
+
+                        animate = FALSE; 
                         break; 
                 }
             }
@@ -126,6 +171,9 @@ int main()
         SDL_RenderPresent(renderer);
         SDL_Delay(1);
     }
+   
+    fclose(write_attractor);
+    fclose(read_attractor);
 
     SDL_DestroyWindow(window);
     SDL_Quit();
